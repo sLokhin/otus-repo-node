884bb56ab2fb4c0527513cf97f340fe71643e39c
sLokhin <sl@happynumbers.com>
2024-05-07 19:47:19 +0300

    feat: add build tree function

---
+++ tree.js
@@ -0,0 +1,53 @@
+const symbols = ['├', '│', '└'];
+const space = '\u0020';
+const newLine = '\u000A';
+
+const data = {
+  name: 1,
+  items: [
+    {
+      name: 2,
+      items: [{ name: 3 }, { name: 4 }],
+    },
+    {
+      name: 5,
+      items: [{ name: 6 }],
+    },
+  ],
+};
+
+function buildTree(data) {
+  const { name, items } = data;
+
+  const root = Boolean(String(name)) ? `${name}${newLine}` : `Root${newLine}`;
+
+  if (!hasChildItems(data)) return root;
+
+  return root + buildBranch(items);
+}
+
+function buildBranch(items, prefix = '') {
+  let branch = '';
+
+  items.forEach((item, i) => {
+    const isLastItem = i === items.length - 1;
+    const nextSymbol = isLastItem ? symbols[2] : symbols[0];
+    branch += prefix + nextSymbol + item.name + newLine;
+
+    if (hasChildItems(item))
+      branch += buildBranch(
+        item.items,
+        prefix + (isLastItem ? space : symbols[1])
+      );
+  });
+
+  return branch;
+}
+
+function hasChildItems(item) {
+  const { items } = item;
+  return Array.isArray(items) && items.length > 0;
+}
+
+console.log('Build tree function');
+console.log(buildTree(data));
