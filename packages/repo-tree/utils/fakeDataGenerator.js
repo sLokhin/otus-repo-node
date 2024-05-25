ecbaed2c763c7ef47435de8884ad89abc6021781
sLokhin <sl@happynumbers.com>
2024-05-25 18:56:07 +0300

    feat: install faker, add fakeDataGenerator

---
+++ packages/repo-tree/utils/fakeDataGenerator.js
@@ -0,0 +1,43 @@
+import { faker } from '@faker-js/faker';
+import { buildTree } from './tree.js';
+
+function getRandomFileName() {
+  return faker.system.fileName({
+    extensionCount: { min: 1, max: 2 },
+  });
+}
+
+function getRandomFolderName() {
+  return faker.word.noun({
+    length: { min: 5, max: 10 },
+    strategy: 'longest',
+  });
+}
+
+function getRandomNumber(min, max) {
+  return faker.number.int({ min, max });
+}
+
+function getRandomBranch(currentDepth, maxDepth) {
+  return {
+    name: currentDepth ? getRandomFolderName() : 'Root',
+    items: new Array(getRandomNumber(1, 3)).fill(null).map(() => {
+      if (currentDepth < maxDepth) {
+        return getRandomBranch(currentDepth + 1, maxDepth);
+      } else {
+        return { name: getRandomFileName() };
+      }
+    }),
+  };
+}
+
+function getRandomData(maxDepth = 0) {
+  return getRandomBranch(0, maxDepth);
+}
+
+const randomData = getRandomData(3);
+
+console.log(randomData);
+console.log(buildTree(randomData));
+
+export { getRandomData };
