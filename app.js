4349edae594460ed84dc5151a7262ae16a971626
sLokhin <sl@happynumbers.com>
2024-06-01 23:18:12 +0300

    feat: add ejs, index.ejs

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const __dirname = path.resolve();
 const PORT = process.env.PORT ?? 3000;
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'static')));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Express' });
+});
 
 app.listen(PORT, () => {
   console.log(`Server has been started on port ${PORT}`);
