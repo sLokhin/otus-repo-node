cb7dcc4a3411457b3bb7e24cae6faaaf3532f15c
sLokhin <sl@happynumbers.com>
2024-06-24 22:27:58 +0300

    feat: add comments endpoints

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ import rolesRouter from './routes/roles.js';
 import usersRouter from './routes/users.js';
 import lessonsRouter from './routes/lessons.js';
 import coursesRouter from './routes/courses.js';
+import commentsRouter from './routes/comments.js';
 
 const __dirname = path.resolve();
 const PORT = process.env.PORT ?? 3000;
@@ -57,6 +58,7 @@ app.use('/api/roles', rolesRouter.apiRouter);
 app.use('/api/users', usersRouter.apiRouter);
 app.use('/api/lessons', lessonsRouter.apiRouter);
 app.use('/api/courses', coursesRouter.apiRouter);
+app.use('/api/comments', commentsRouter.apiRouter);
 
 app.use((req, res, next) => {
   next(createHttpError(404));
