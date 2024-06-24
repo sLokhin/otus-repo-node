e954ae26303517e29cb809fd7c3182b59c92963e
sLokhin <sl@happynumbers.com>
2024-06-24 23:08:48 +0300

    feat; remove role class

--- classes/index.js
+++ classes/index.js
@@ -1,7 +1,6 @@
-import Role from './role.js';
 import User from './user.js';
 import UserRepository from './userRepository.js';
 import Course from './course.js';
 import CourseRepository from './courseRepository.js';
 
-export { Role, User, UserRepository, Course, CourseRepository };
+export { User, UserRepository, Course, CourseRepository };
--- classes/role.js
+++
@@ -1,9 +0,0 @@
-class Role {
-  constructor(id, name, description) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-  }
-}
-
-export default Role;
